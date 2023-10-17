95c77c80e695e9ca0df3ad17d21fa05c9fab2e42
20ml <milliliter.avc@gmail.com>
2023-10-17 13:58:32 -0400

    Create rudimentary audio system

---
+++ scripts/audio/AudioDispatcher.gd
@@ -0,0 +1,52 @@
+#class_name AudioDispatcher
+extends Node
+
+var currently_playing_audios : Dictionary = {}
+var currently_ending_audios : Dictionary = {}
+
+func _get_audio_stream(audio_name : StringName) -> AudioStream:
+	return load("res://assets/audio/" + audio_name + "/randomizer.tres")
+
+func _free_audioplayer(audio_name : String):
+	if not currently_playing_audios.has(audio_name): return
+	var stream = currently_playing_audios[audio_name]
+	stream.queue_free()
+	currently_playing_audios.erase(audio_name)
+	currently_ending_audios.erase(audio_name)
+
+func dispatch_audio(parent : Node, audio_name : StringName) -> AudioStreamPlayer:
+	var stream := AudioStreamPlayer.new()
+	if currently_playing_audios.has(audio_name):
+		stream = currently_playing_audios[audio_name]
+		if currently_ending_audios.has(audio_name):
+			currently_ending_audios[audio_name].kill()
+	else:
+		stream.stream = _get_audio_stream(audio_name)
+		parent.add_child(stream)
+	stream.play()
+	stream.volume_db = 0
+	stream.finished.connect(func(): _free_audioplayer(audio_name))
+	return stream
+
+func dispatch_3d_audio(parent : Node, audio_name : StringName) -> AudioStreamPlayer3D:
+	var stream := AudioStreamPlayer3D.new()
+	if currently_playing_audios.has(audio_name):
+		stream = currently_playing_audios[audio_name]
+		if currently_ending_audios.has(audio_name):
+			currently_ending_audios[audio_name].kill()
+	else:
+		stream.stream = _get_audio_stream(audio_name)
+		currently_playing_audios[audio_name] = stream
+		parent.add_child(stream)
+	stream.play()
+	stream.volume_db = 0
+	stream.finished.connect(func(): _free_audioplayer(audio_name))
+	return stream
+
+func end(audio_name : StringName, fadeout_time : float = 0.0):
+	if not currently_playing_audios.has(audio_name): return
+	var stream = currently_playing_audios[audio_name]
+	var tween = get_tree().create_tween()
+	tween.tween_property(stream, "volume_db", -100, fadeout_time)
+	tween.tween_callback(_free_audioplayer.bind(audio_name))
+	currently_ending_audios[audio_name] = tween
